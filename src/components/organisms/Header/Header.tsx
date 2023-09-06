5587431eba9e84e26edf42a6a438611598a60446
Tam Nguyen <thanhtam.nguyen@planet1world.com>
2023-09-06 11:30:13 +0700

    test: styled-component SRR

---
+++ src/components/organisms/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+// import HeaderStyled from "./HeaderStyled";
+
+interface HeaderProps {}
+
+const Header: React.FC<HeaderProps> = ({}) => {
+  return <div>123</div>;
+};
+
+export default Header;
